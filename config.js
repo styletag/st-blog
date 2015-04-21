c113516b48f88841ae4dd55017d2e4fec6700dfb
Prashanth HN <prashanth@prashanth.net>
2015-04-21 12:47:19 +0530

    config

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://tagit.styletag.com',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -24,7 +24,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '80'
         }
     },
 
